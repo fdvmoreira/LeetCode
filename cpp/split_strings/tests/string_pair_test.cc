335e17a15c3a5f5c07ef50d6e36d5079191e6e1f
Flavio Moreira <mr.flaviomoreira@gmail.com>
2022-07-05 22:32:25 +0100

    rewrite the string splitter cmake files to remove redundancy

---
+++ cpp/split_strings/tests/string_pair_test.cc
@@ -0,0 +1,58 @@
+//
+// Created by Flavio Moreira on 04/07/22.
+//
+
+#include <gtest/gtest.h>
+#include <vector>
+#include <string>
+#include "./../src/include/string_pair.h"
+
+TEST(StringPairTest, TestStringPairEventLength){
+    /**
+     *
+
+    auto it = solution("abcdef").begin();
+    for (auto res : {"ab", "cd", "ef"}) {
+        EXPECT_EQ(res,it);
+        it++;
+    }
+     *
+     */
+    ASSERT_EQ("a","a");
+    //ASSERT_EQ(solution("abcdef"),std::vector<std::string>{"ab", "cd", "ef"})<<"Should pass";
+    //ASSERT_NE("ab",("b"))<<"Should pass";
+}
+
+TEST(StringPairTest, TestStringPairOddtLength){
+    //ASSERT_THAT(solution("abcde"),({"ab", "cd", "e_"}))<<"Should return {\"ab\", \"cd\", \"e_\"}";
+    //ASSERT_EQ(solution("LovePizza"), {"Lo", "ve", "Pi", "zz", "a_"})<<"Should pass";
+}
+
+/**
+*  TEST CASES
+ *
+ * Describe(sample_test)
+{
+    It(even_length)
+    {
+       do_test("abcdef", 0000000);
+       do_test("HelloWorld", {"He", "ll", "oW", "or", "ld"});
+    }
+    It(odd_length)
+    {
+       do_test("abcde", {"ab", "cd", "e_"});
+       do_test("LovePizza", {"Lo", "ve", "Pi", "zz", "a_"});
+    }
+};
+*/
+
+//          ／＞　　フ
+//　　　　　| 　n　n 彡
+//　 　　　／`ミ＿xノ
+//　　 　 /　　　 　 |
+//　　　 /　 ヽ　　 ﾉ
+//　 　 │　　|　|　|
+//　／￣|　　 |　|　|
+//　| (￣ヽ＿_ヽ_)__)
+//　＼二つ
+// ITS CAT FOR YOU
