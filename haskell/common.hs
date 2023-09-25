5738c2c95e8f1c7751a461cab17d642f86b30078
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-09-25 20:10:07 +0100

    implement filter function

--- haskell/common.hs
+++ haskell/common.hs
@@ -3,6 +3,23 @@ module Common
   )
 where
 
+import Distribution.Simple.Utils (xargs)
+
 map' :: (a -> b) -> [a] -> [b]
 map' _ [] = []
 map' fn (x : xs) = fn x : map' fn xs
+
+-- usage: map' (**2) [43,2,34,55]
+-- output [1849.0,4.0,1156.0,3025.0]
+
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' f (x : xs)
+  | f x = x : filter' f xs
+  | otherwise = filter' f xs
+
+-- usage:
+-- filter' (>10) [5..15]
+-- output> [11,12,13,14,15]
+--
