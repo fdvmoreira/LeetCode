8117c0dea67c9184ec704171915c085b8e51e67d
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-09-26 09:04:01 +0100

    implement quicksort algorithm with filter' function

--- haskell/common.hs
+++ haskell/common.hs
@@ -12,7 +12,6 @@ map' fn (x : xs) = fn x : map' fn xs
 -- usage: map' (**2) [43,2,34,55]
 -- output [1849.0,4.0,1156.0,3025.0]
 
-
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' _ [] = []
 filter' f (x : xs)
@@ -22,4 +21,16 @@ filter' f (x : xs)
 -- usage:
 -- filter' (>10) [5..15]
 -- output> [11,12,13,14,15]
+
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x : xs) =
+  let smallerSorted = quicksort' (filter' (<= x) xs)
+      biggerSorted = quicksort' (filter' (> x) xs)
+   in smallerSorted ++ [x] ++ biggerSorted
+
+-- usage
+-- quicksort' [100,90..0]
+-- output: [0,10,20,30,40,50,60,70,80,90,100]
+--
 --
