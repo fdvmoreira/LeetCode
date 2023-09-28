6f5ccdf52c6fa974682789d859628eac101f98d5
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-09-28 22:55:58 +0100

    Implement the sum the square of all natural numbers under 10000

--- haskell/challenges.hs
+++ haskell/challenges.hs
@@ -1,4 +1,4 @@
-module Challenges (largestDivisibleBy3829) where
+module Challenges (largestDivisibleBy3829, oddSquareUnder10k) where
 
 import Common qualified
 
@@ -9,3 +9,9 @@ largestDivisibleBy3829 :: (Integral a) => a
 largestDivisibleBy3829 = head (Common.filter' f [100000, 99999 ..])
   where
     f x = x `mod` 3829 == 0
+
+--
+-- Sum all odd squares less than 10000
+--
+oddSquareUnder10k :: (Integral a) => a
+oddSquareUnder10k = sum (takeWhile (< 10000) (Common.filter' odd (Common.map' (^ 2) [1 ..])))
