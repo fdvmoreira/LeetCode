00011b232fc6b58fe8adb0b5d536a47f2a5063a8
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-12-03 19:21:04 +0000

    Restructure the project separate days into files

--- aoc-2023/src/main.rs
+++ aoc-2023/src/main.rs
@@ -1,8 +1,8 @@
-use aoc_2023::day1;
+use aoc_2023::day1::day1;
 
 fn main() {
     println!(
         "{}",
-        day1::trubuchet(format!("data/day1.txt").as_str()).unwrap()
+        day1::trubuchet_one(format!("data/day1.txt").as_str()).unwrap()
     );
 }
