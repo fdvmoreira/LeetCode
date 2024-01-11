7caff8c0c9a09089f6de5b25ae118cb818f431cf
Flavio Moreira <mr.flaviomoreira@gmail.com>
2024-01-11 13:15:13 +0000

    Add required text brief for the exercise and layout initial test

--- aoc-2023/src/main.rs
+++ aoc-2023/src/main.rs
@@ -1,10 +1,9 @@
-use aoc_2023::day9_part_two::extrapolated_sum;
+use aoc_2023::day10::total_steps;
 use aoc_2023::utils::load_file_content;
 
 fn main() {
     let data = load_file_content("data/day9.txt").unwrap();
     println!("Calculating ...");
-    let ways =
-        extrapolated_sum(&data.iter().map(|row| row.as_str()).collect::<Vec<&str>>()).unwrap();
-    println!("Result {:?}", ways);
+    let steps = total_steps(&data.iter().map(|row| row.as_str()).collect::<Vec<&str>>()).unwrap();
+    println!("Result {:?}", steps);
 }
