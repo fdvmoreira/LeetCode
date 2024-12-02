feb152f2e8a14a0e690c8bb8b97755f1e5dfbcbe
Flavio Moreira <mr.flaviomoreira@gmail.com>
2024-12-02 20:05:47 +0000

    AoC24: add test and testing data

---
+++ aoc-2024/go/day2/day2_test.go
@@ -0,0 +1,22 @@
+package day2_test
+
+import (
+	. "aoc/day2"
+	. "aoc/utils"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCountSafeLevels(t *testing.T) {
+	data, err := ReadFile("./test_data.txt")
+	if !assert.NoError(t, err) {
+		assert.Fail(t, err.Error())
+	}
+
+	const expected = 2
+	actual, err := CountSafeLevels(data)
+	if assert.NoError(t, err) {
+		assert.Equal(t, expected, actual)
+	}
+}
