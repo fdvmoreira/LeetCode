9634864c5689e4b87c88bce05f7c64c2bfca2e93
Flavio Moreira <mr.flaviomoreira@gmail.com>
2024-12-03 14:55:03 +0000

    AoC24-2-2:  Setup second part challenge; NOTE: it is incomplete though

--- aoc-2024/go/day2/day2.go
+++ aoc-2024/go/day2/day2.go
@@ -2,6 +2,7 @@ package day2
 
 import (
 	// "fmt"
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -62,6 +63,69 @@ func CountSafeLevels(reports []string) (int, error) {
 			}
 
 			return true
+		}(report)
+
+		if safe {
+			count += 1
+		}
+	}
+
+	return count, nil
+}
+
+func CountSafeLevelsWithTolerance(reports []string) (int, error) {
+	count := 0
+
+	for _, report := range reports {
+
+		safe := func(report string) bool {
+			levels := strings.Fields(report)
+			var iLevels []int = make([]int, len(levels))
+
+			for idx, v := range levels {
+				val, err := strconv.Atoi(v)
+				if err != nil {
+					panic(err)
+				}
+				iLevels[idx] = val
+			}
+
+			var initialOrder = Increasing
+			const maxAdjecentLevelDistance = 3
+			const minAdjacentLevelDistance = 1
+
+			// determine the correct order in the list
+			if first, second := iLevels[0], iLevels[1]; first > second {
+				initialOrder = Decreasing
+			}
+
+			badLevelCounter := 0
+
+			for i := 1; i < len(iLevels); i++ {
+				lastLevel, currentLevel := iLevels[i-1], iLevels[i]
+				currentAdjacentLevelDistance := currentLevel - lastLevel
+
+				if initialOrder == Decreasing {
+					currentAdjacentLevelDistance = lastLevel - currentLevel
+				}
+
+				currentOrder := initialOrder
+
+				// determine if the order is ascending or descending according to the first and second level
+				if firstLevel, secondLevel := iLevels[i-1], iLevels[i]; firstLevel > secondLevel {
+					currentOrder = Decreasing
+				} else {
+					currentOrder = Increasing
+				}
+
+				if (currentAdjacentLevelDistance < minAdjacentLevelDistance) || (currentAdjacentLevelDistance > maxAdjecentLevelDistance) || (currentOrder != initialOrder) {
+					badLevelCounter += 1
+					//TODO: Remove the bad level once and reiterate
+					fmt.Printf("x")
+				}
+			}
+
+			return (badLevelCounter <= 1)
 		}(report)
 
 		if safe {
--- aoc-2024/go/day2/day2_test.go
+++ aoc-2024/go/day2/day2_test.go
@@ -14,8 +14,21 @@ func TestCountSafeLevels(t *testing.T) {
 		assert.Fail(t, err.Error())
 	}
 
-	const expected = 3
+	const expected = 2
 	actual, err := CountSafeLevels(data)
+	if assert.NoError(t, err) {
+		assert.Equal(t, expected, actual)
+	}
+}
+
+func TestCountSafeLevelsWithTolerance(t *testing.T) {
+	data, err := ReadFile("./test_data.txt")
+	if !assert.NoError(t, err) {
+		assert.Fail(t, err.Error())
+	}
+
+	const expected = 4
+	actual, err := CountSafeLevelsWithTolerance(data)
 	if assert.NoError(t, err) {
 		assert.Equal(t, expected, actual)
 	}
