af1b5b04da034763f9b64e8ced9a80d0f9b6baae
Flavio Moreira <mr.flaviomoreira@gmail.com>
2024-12-07 18:57:48 +0000

    AoC24-5: Setup day extra test and function to check ordering

--- aoc-2024/go/day5/day5.go
+++ aoc-2024/go/day5/day5.go
@@ -1,6 +1,13 @@
 package day5
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math"
+	"slices"
+	"strconv"
+	"strings"
+)
 
 func SumMiddlePages(data []string) (int, error) {
 	sum := 0
@@ -9,5 +16,54 @@ func SumMiddlePages(data []string) (int, error) {
 		return sum, nil
 	}
 
+	var (
+		rules   []string
+		updates [][]int
+	)
+
+	_ = rules
+
+	sectionPlitIndex := slices.Index(data, "")
+
+	if sectionPlitIndex == -1 {
+		return sum, errors.New("Section Devision/Split Error")
+	}
+
+	// split the data into rules and updates
+	for _, line := range data[sectionPlitIndex+1:] {
+
+		// transform the string into array of numbers
+		pages := func(str *string) (arr []int) {
+			tmpNum := strings.Split(*str, ",")
+			for _, v := range tmpNum {
+				num, err := strconv.Atoi(v)
+				if err != nil {
+					fmt.Printf("Error: %v\n", err.Error())
+				}
+				arr = append(arr, num)
+			}
+
+			return
+
+		}(&line)
+
+		updates = append(updates, pages)
+	}
+
+	for _, update := range updates {
+		if IsPageOrdered(update) {
+			middleIndex := int(math.Floor(float64(len(update) / 2)))
+			sum += update[middleIndex]
+		}
+	}
+
 	return sum, nil
+}
+
+func IsPageOrdered(pages []int) bool {
+	if len(pages) == 0 {
+		return false
+	}
+
+	return true
 }
--- aoc-2024/go/day5/day5_test.go
+++ aoc-2024/go/day5/day5_test.go
@@ -22,3 +22,21 @@ func TestSumMiddlePages(t *testing.T) {
 	}
 	assert.Equal(t, expected, actual)
 }
+
+func TestIsPagesOrdered(t *testing.T) { // TODO: Add Fuzz testing or change to table/data drive test
+	var (
+		p1Expected = []int{75, 47, 61, 53, 29}
+		p2Expected = []int{97, 61, 53, 29, 13}
+		p3Expected = []int{75, 29, 13}
+		p4Expected = []int{75, 97, 47, 61, 53}
+		p5Expected = []int{61, 13, 29}
+		p6Expected = []int{97, 13, 75, 29, 47}
+	)
+
+	assert.True(t, IsPageOrdered(p1Expected))
+	assert.True(t, IsPageOrdered(p2Expected))
+	assert.True(t, IsPageOrdered(p3Expected))
+	assert.False(t, IsPageOrdered(p4Expected))
+	assert.False(t, IsPageOrdered(p5Expected))
+	assert.False(t, IsPageOrdered(p6Expected))
+}
