ce11c664f76126af6ab8cd86c89312cae460ec00
Flavio Moreira <mr.flaviomoreira@gmail.com>
2024-12-17 16:26:55 +0000

    Update the solution plan

--- aoc-2024/go/day12/day12.go
+++ aoc-2024/go/day12/day12.go
@@ -17,7 +17,12 @@ func CalculateFencePrice(garden *[]string) (int, error) {
 	// Find start of each group
 	// Find the height and width of each group
 	// NOTE: Mark the visited location to prevent double
-	// count the marked location (as these should ne unique for each group)
+	// count the marked location (as these should ne unique for each group). This can be done linearly by using a map
+	// ---
+	// Another alternative
+	// ---
+	// Scan line by line  and determine the present group, the start and end, check if the previous group ended
+	//
 
 	return totalFencePrice, nil
 }
